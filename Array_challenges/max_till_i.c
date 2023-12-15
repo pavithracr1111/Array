45624c872f1f6736a7e35773268587bf071c137d
pavi <pavithracr2001@gmail.com>
2023-12-15 21:48:58 +0530

    altered

--- Array_challenges/max_till_i.c
+++ Array_challenges/max_till_i.c
@@ -1,16 +1,14 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 int main()
 {
-    int n=6;
-    int arr[]={0,-9,1,3,-4,5};
-    int mx=-1999999;
+    int n = 6;
+    int arr[] = {0, -9, 1, 3, -4, 5};
+    int mx = -1999999;
 
-   for(int i=0;i<n;i++)
-   {
-        mx=fmax(mx,arr[i]);     //fmax for floating point numbers and max for integers
-        printf("%d ",mx);
-   }
-   
-    
+    for (int i = 0; i < n; i++)
+    {
+        mx = fmax(mx, arr[i]); // fmax for floating point numbers and max for integers
+        printf("%d ", mx);
+    }
 }
